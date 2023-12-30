4cfa2041e2b2db14f0c49ff7c0a7b064a646c05f
Intizar Tashov <intizar.tashov22@gmail.com>
2023-12-31 02:41:38 +0300

    add dashboard changes

---
+++ app/server/dbClient.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5440'),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export const dbClient = drizzle(pool);
